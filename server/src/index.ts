09d13b830ae033aaa082f3aaa0aa306c3ef893a3
Maxym Rybak <hurubashi@gmail.com>
2020-06-22 18:27:51 +0300

    update docker-compose, add Dockerfile for server, sequalize installed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import { Sequelize } from "sequelize"
 
 const app = express()
 
@@ -10,3 +11,19 @@ app.listen(5000, (err, res) => {
 	console.log(err)
 	console.log(res)
 })
+
+const sequelize = new Sequelize("db", "user", "pass", {
+	host: "postgres",
+	dialect: "postgres",
+})
+
+async function testDb() {
+	try {
+		await sequelize.authenticate()
+		console.log("Connection has been established successfully.")
+	} catch (error) {
+		console.error("Unable to connect to the database:", error)
+	}
+}
+
+testDb()
