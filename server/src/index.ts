11136ce99f64a70fe88c3dea39e07223e5b75a93
Maxym Rybak <hurubashi@gmail.com>
2020-06-23 23:29:47 +0300

    add dockerignore files. update Dockerfiles, docker-compose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
 import express from "express"
+import { PrismaClient } from "@prisma/client"
 
 const app = express()
 
-app.use("/", (req, res) => {
-	return res.status(200).json({ greeting: "hello" })
+const prisma = new PrismaClient()
+
+async function getUsers() {
+	const users = await prisma.user.findMany({
+		include: {
+			chat: true,
+		},
+	})
+
+	return users
+}
+
+app.use("/", async (req, res) => {
+	const users = await getUsers()
+	return res.status(200).json({ users: users })
 })
 
 app.listen(5000, (err, res) => {
