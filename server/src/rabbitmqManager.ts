6e619aa54295797032b467480d17e74d04487855
Maxym Rybak <hurubashi@gmail.com>
2020-06-25 10:28:28 +0300

    client modules returned to ignore, rabbitmqManager example functions

--- server/src/rabbitmqManager.ts
+++ server/src/rabbitmqManager.ts
@@ -1,15 +1,26 @@
 import ampq from "amqplib"
 
-async function connect() {
+async function consumer() {
 	try {
 		const connection = await ampq.connect("amqp://rabbitmq:5672")
 		const channel = await connection.createChannel()
 		await channel.assertQueue("jobs")
 		channel.consume("jobs", (message) => {
 			if (message) {
-				const input = JSON.parse(message.content.toString())
+				console.log(message.content)
 			}
 		})
 	} catch (ex) {}
 }
-connect()
+
+async function publisher(msg: Object) {
+	try {
+		const connection = await ampq.connect("amqp://localhost:5672")
+		const channel = await connection.createChannel()
+		await channel.assertQueue("jobs")
+		channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)))
+		console.log(`Job sent successfully ${msg}`)
+	} catch (ex) {
+		console.error(ex)
+	}
+}
